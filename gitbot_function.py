5ae5f6b532f1e09ad7b309cb6c60fa36c7e39e94
wreckitkenny <tung.tran.3295@gmail.com>
2021-10-26 17:40:51 +0700

    Update hotfix on 0.1.6

--- gitbot_function.py
+++ gitbot_function.py
@@ -23,19 +23,19 @@ def gitBot(resource, configPath, binPath):
     if env != '':
         oldTag = getOldTag(cdProject, repoName)
         location = searchFile(cdProject, repoName)
-        branch_list = [branch.name for branch in cdProject.branches.list()]
+        # branch_list = [branch.name for branch in cdProject.branches.list()]
 
         if env == 'release':
-            if env in branch_list: 
-                logging.warning('Branch [{}] is existing on Production.'.format(env))
-                cdProject.branches.delete(env)
-                logging.info('Gitbot has removed old [{}] branch'.format(env))
-            logging.info('Gitbot is creating a new [{}] branch'.format(env))
-            cdProject.branches.create({'branch': env, 'ref': 'master'})
+            # if env in branch_list: 
+            #     logging.warning('Branch [{}] is existing on Production.'.format(env))
+            #     cdProject.branches.delete(env)
+            #     logging.info('Gitbot has removed old [{}] branch'.format(env))
+            logging.info('Gitbot is creating a new [{}] branch'.format(newTag))
+            cdProject.branches.create({'branch': newTag, 'ref': 'master'})
             branchName = env
 
         if oldTag != '':
             logging.info('GitBot is comparing old tag [{}] to new tag [{}].'.format(oldTag, newTag))
             if (lambda x,y: (x>y)-(x<y))(oldTag,newTag) == 0: logging.info("==> No tag changed!!!")
-            changeTag(gl, cdProject, oldTag, newTag, binPath, location, branchName)
+            changeTag(gl, resource, cdProject, oldTag, newTag, binPath, location, branchName)
     else: logging.error("==> The image [{}] is rejected to deploy.".format(resource))
--- gitbot_module.py
+++ gitbot_module.py
@@ -7,7 +7,7 @@ def changeContent(file, old, new):
     with open(file, 'w') as f:
         f.write(content)
 
-def changeTag(gl, cdProject, oldTag, newTag, binPath, location, branchName):
+def changeTag(gl, resource, cdProject, oldTag, newTag, binPath, location, branchName):
     # Check directory existing
     cdFolder = '/'.join(location[0].split('/')[:-1])
     if os.path.isdir(binPath+'/'+cdFolder) == False: os.makedirs(binPath+'/'+cdFolder)
@@ -23,7 +23,7 @@ def changeTag(gl, cdProject, oldTag, newTag, binPath, location, branchName):
 
     # Commit changes
     data = {
-        'branch': branchName,
+        'branch': newTag,
         'commit_message': 'Change tag',
         'actions': [
             {
@@ -33,14 +33,14 @@ def changeTag(gl, cdProject, oldTag, newTag, binPath, location, branchName):
             }
         ]
     }
-    logging.info('Gitbot is committing new change for new tag [{}] to branch [{}].'.format(newTag, branchName))
+    logging.info('Gitbot is committing new change to branch [{}].'.format(newTag))
     cdProject.commits.create(data)
 
     # Create merge request
     if branchName == 'release':
         assignees = getApprovers(gl, cdProject, cdFolder)
-        logging.info('Gitbot is creating a merge request for new branch [{}]'.format(branchName))
-        mr = cdProject.mergerequests.create({'source_branch':branchName, 'target_branch':'master', 'title':'Merge new version to production', 'assignee_ids':assignees})
+        logging.info('Gitbot is creating a merge request for new branch [{}]'.format(newTag))
+        mr = cdProject.mergerequests.create({'source_branch':newTag, 'target_branch':'master', 'title':'Vnpaybot has released {}'.format(resource), 'assignee_ids':assignees})
         mr.approval_rules.create({"name": "Production MR Policy", "approvals_required": 2, "rule_type": "regular","user_ids": assignees})
 
     # Complete
