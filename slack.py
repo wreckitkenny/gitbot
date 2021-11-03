5581735014867334c0a3a268ef41728ca31bd1ad
Tran Thanh Tung <tungtt4@vnpay.vn>
2021-11-03 18:11:59 +0700

    Update proxy

--- slack.py
+++ slack.py
@@ -1,6 +1,12 @@
 import os
 from slack_sdk import WebClient
 
+proxy = 'http://10.20.23.210:9090'
+os.environ['http_proxy'] = proxy 
+os.environ['HTTP_PROXY'] = proxy
+os.environ['https_proxy'] = proxy
+os.environ['HTTPS_PROXY'] = proxy
+
 def slack(token, channel, app, msg):
     # slack_token = os.environ["SLACK_BOT_TOKEN"]
     slack_token = token
